b40830b801fa8aaca6990ca53ac76162d9f68bc0
black-RAM <pplanes311@gmail.com>
2023-08-18 12:18:47 +0300

    Attempt to set up SASS, ran into error [200~Error: Conflict: Multiple chunks emit assets to the same filename main.js (chunks index and runtime)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
   mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: './dist'
+  },
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  // to use SASS
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+            },
+          },
+        ]
+      }
+    ]
+  }
 };

6d0b982bd219be8653e104ea94c0fc2872331918
black-RAM <pplanes311@gmail.com>
2023-08-29 20:45:03 +0300

    now using the HTML webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './style.scss'
 import renderAboutPage from "./about-page"
-renderAboutPage()
+
+// setup: insert the content div
+const contentDiv = document.createElement("div");
+contentDiv.id = "content";
+document.body.appendChild(contentDiv);
+
+// Call the renderAboutPage function
+renderAboutPage();
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Kaj's Kitchen"
+    })
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devServer: {
     static: './dist'
@@ -31,6 +38,14 @@ module.exports = {
             },
           },
         ]
+      },
+      { // to allow loading images in JS
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      { // to load font files
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ]
   }
